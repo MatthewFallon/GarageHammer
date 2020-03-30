fe8c3cea34917166d2f379db5f0f0179377a31c8
Matthew Fallon <matthew.b.fallon@gmail.com>
2020-03-30 04:04:12 -0500

    Adding the finishing touches to make links functional and neat. Removed erroneous symbols.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -3,15 +3,18 @@ window.onload = function () {
      * @type HTMLDivElement
      * */
     var navbar = document.querySelector("header nav");
+    var sectionTop = document.querySelector("main > article, main > section");
 
     var stickyHeight = navbar.offsetTop;
+    var stickyLength = navbar.clientHeight;
 
     window.onscroll = function () {
-        console.log(navbar);
         if (window.pageYOffset >= stickyHeight) {
             navbar.classList.add("sticky");
+            sectionTop.style = "padding-top: " + ( stickyLength + 16) + "px;";
         } else {
             navbar.classList.remove("sticky");
+            sectionTop.style = "";
         }
     }
 }
